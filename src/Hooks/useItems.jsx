50efe3c7353918eb0a6a578ac88bdd6322d374ef
hexaalif <shadullahsad9@gmail.com>
2024-05-23 14:21:06 +0600

    product view authorization added

--- src/Hooks/useItems.jsx
+++ src/Hooks/useItems.jsx
@@ -9,9 +9,9 @@ const useItems = () => {
     const getItems = async () => {
       try {
         const res = await axios.get("http://127.0.0.1:8000/list/", {
-          headers: {
-            Authorization: `token ${localStorage.getItem("token")}`,
-          },
+          // headers: {
+          //   Authorization: `token ${localStorage.getItem("token")}`,
+          // },
         });
         setItems(res.data);
       } catch (err) {
--- src/component/Pages/Profile/Profile.jsx
+++ src/component/Pages/Profile/Profile.jsx
@@ -1,7 +1,15 @@
 const Profile = () => {
   return (
     <div>
-      <h1>My Profile</h1>
+      <h1 className="py-24 text-4xl font-bold text-center bg-gray-400/50">
+        My Profile
+      </h1>
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };

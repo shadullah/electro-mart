4c6769c25710ceedfdc5a428fd9442e102cfcf6a
hexaalif <shadullahsad9@gmail.com>
2024-05-26 19:09:40 +0600

    gellary responsive

--- src/component/Pages/Home/GellaryList/GellaryList.jsx
+++ src/component/Pages/Home/GellaryList/GellaryList.jsx
@@ -3,18 +3,18 @@ import "./GellaryList.css";
 
 const GellaryList = () => {
   return (
-    <div className="max-w-[1200px] mx-auto my-16">
+    <div className="max-w-[1200px] mx-4 md:mx-auto my-16 ">
       <div
-        className="grid grid-cols-3 gap-4"
-        style={{ gridTemplateColumns: "1fr 2fr 1fr" }}
+        className="grid grid-cols-1 md:grid-cols-3 gap-4 md:grid-template-columns-[1fr_2fr_1fr]"
+        // style={{ gridTemplateColumns: "1fr 2fr 1fr" }}
       >
-        <div className="bg p-10 col-span-1">
+        <div className="bg p-10 md:col-span-1">
           <h1 className="text-4xl whitespace-nowrap">
             Smart <span className="font-bold">Watch</span>
           </h1>
           <p className="pb-12 pt-2">0 Products</p>
           <div className="py-24"></div>
-          <button className="flex  items-center mt-6">
+          <button className="flex items-center mt-6">
             Shop Now{" "}
             <span className="ml-3">
               <FaArrowRightLong />
@@ -23,20 +23,20 @@ const GellaryList = () => {
         </div>
 
         <div className="col-span-1 grid grid-rows-2 gap-4 text-white">
-          <div className="bg2 p-10 row-span-1">
+          <div className="bg2 p-10 col-span-1">
             <h1 className="text-4xl">
               Quality <span className="font-bold">Keyboard</span>
             </h1>
             <p className="pb-12 pt-2">0 Products</p>
             <div className="px-24"></div>
-            <button className="flex  items-center mt-6">
+            <button className="flex items-center mt-6">
               Shop Now{" "}
               <span className="ml-3">
                 <FaArrowRightLong />
               </span>{" "}
             </button>
           </div>
-          <div className="bg3 p-10 row-span-1 text-end">
+          <div className="bg3 p-10 col-span-1 text-end">
             <h1 className="text-4xl">
               Wireless <span className="font-bold">Headphone</span>
             </h1>

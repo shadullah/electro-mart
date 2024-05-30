622379ae758faadc66030bb6095a5b043819cf44
hexaalif <shadullahsad9@gmail.com>
2024-05-30 20:03:29 +0600

    trying url loops

--- src/Hooks/useItems.jsx
+++ src/Hooks/useItems.jsx
@@ -4,24 +4,30 @@ import { useEffect, useState } from "react";
 const useItems = () => {
   const [loading, setLoading] = useState(true);
   const [items, setItems] = useState([]);
+  const urls = [
+    "https://work-notes-server.onrender.com/todo/list/",
+    "http://127.0.0.1:8000/list/",
+  ];
 
   useEffect(() => {
     const getItems = async () => {
-      try {
-        const res = await axios.get("http://127.0.0.1:8000/list/", {
-          // headers: {
-          //   Authorization: `token ${localStorage.getItem("token")}`,
-          // },
-        });
-        setItems(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
+      for (const url of urls) {
+        try {
+          const res = await axios.get(url, {
+            // headers: {
+            //   Authorization: `token ${localStorage.getItem("token")}`,
+            // },
+          });
+          setItems(res.data);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
       }
     };
     getItems();
-  }, []);
+  }, [urls]);
 
   return [items, loading];
 };

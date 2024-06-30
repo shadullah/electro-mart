f7275b2623960c6ed50c75684b8423550f3f904e
hexaalif <shadullahsad9@gmail.com>
2024-06-30 11:36:03 +0600

    category filter function more efficient

--- src/Hooks/useItems.jsx
+++ src/Hooks/useItems.jsx
@@ -4,19 +4,21 @@ import { useEffect, useState } from "react";
 const useItems = () => {
   const [loading, setLoading] = useState(true);
   const [items, setItems] = useState([]);
-  const [search, setSearch] = useState("Watch");
+  const [search, setSearch] = useState("");
   const urls = [
     "https://electro-mart-backend.onrender.com/list/",
-    "https://electro-mart-backend.up.railway.app/list/",
-    "http://127.0.0.1:8000/list/",
+    // "https://electro-mart-backend.up.railway.app/list/",
+    // "http://127.0.0.1:8000/list/",
   ];
 
   useEffect(() => {
     const getItems = async () => {
+      setLoading(true);
       // let success = false;
       for (const url of urls) {
         try {
-          const res = await axios.get(url, {
+          const endpoint = search ? `${url}?search=${search}` : url;
+          const res = await axios.get(endpoint, {
             // headers: {
             //   Authorization: `token ${localStorage.getItem("token")}`,
             // },
@@ -32,29 +34,30 @@ const useItems = () => {
       }
     };
     getItems();
-  }, []);
-
-  useEffect(() => {
-    const handleSearch = async () => {
-      try {
-        const res = await axios.get(
-          `https://electro-mart-backend.onrender.com/list/?search=${search}`,
-          {
-            // headers: {
-            //   Authorization: `token ${localStorage.getItem("token")}`,
-            // },
-          }
-        );
-        setItems(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    handleSearch(search);
   }, [search]);
+
+  // useEffect(() => {
+  //   const handleSearch = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const res = await axios.get(
+  //         `https://electro-mart-backend.onrender.com/list/?search=${search}`,
+  //         {
+  //           // headers: {
+  //           //   Authorization: `token ${localStorage.getItem("token")}`,
+  //           // },
+  //         }
+  //       );
+  //       setItems(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   handleSearch(search);
+  // }, [search]);
 
   return [items, setSearch, loading];
 };

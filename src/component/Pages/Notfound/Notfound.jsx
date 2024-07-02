3ef88212019ab0acc6c55b54472566cd81a2ce8e
hexaalif <shadullahsad9@gmail.com>
2024-07-03 02:07:04 +0600

    not found page modified

--- src/component/Pages/Notfound/Notfound.jsx
+++ src/component/Pages/Notfound/Notfound.jsx
@@ -5,10 +5,11 @@ const Notfound = () => {
   return (
     <div className="my-12">
       <div className="flex justify-center my-12">
-        <FaExclamationTriangle className="text-9xl" />
+        <FaExclamationTriangle className="text-9xl text-orange-500" />
       </div>
       <div className="text-center">
-        <h3 className="text-5xl mb-12 ">Page Not Found</h3>
+        <h1 className="text-6xl font-extrabold mb-6">404</h1>
+        <h3 className="text-3xl mb-12 ">Page Not Found</h3>
         <p>
           <Link to="/" className="px-3 py-4 bg-orange-400 rounded-md font-bold">
             Go Back to Home
